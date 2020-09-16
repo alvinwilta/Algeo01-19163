c6cfedffba283ded188212c8dc9df4219a6ba84b
leomatt547 <leo.matt.547@gmail.com>
2020-09-16 17:02:46 +0700

    Input type

---
+++ src/Matriks.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Writer;
+
+public class matriks {
+    //Konstruktor Matriks
+
+    
+}
