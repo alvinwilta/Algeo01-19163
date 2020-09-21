a2debd2ab874a7f34d6bfa3df1d4e80ec01dfc40
leomatt547 <leo.matt.547@gmail.com>
2020-09-21 12:02:54 +0700

    Add Konstruktor

--- src/Matriks.java
+++ src/Matriks.java
@@ -8,7 +8,34 @@ import java.io.FileWriter;
 import java.io.Writer;
 
 public class matriks {
+
+    int IdxMax = 15;
+    //Atribut indeks
+    int Brs;
+    int Kol;
+    // Deklarasi Matriks
+    float[][] Mat = new float[IdxMax][IdxMax];
+    float[][] Tampung = new float[IdxMax][IdxMax];
+
+    //Scanner
+    Scanner input = new Scanner(System.in);
+
     //Konstruktor Matriks
+    matriks(){
+    /* Konstruktor matriks, membuat matriks berukuran IdxMax x IdxMax,
+    kemudian mengisinya dengan 0 dan inisialisasi Baris dan Kolom efektif*/
+        int i,j;
+
+        for (i = 1; i < IdxMax; i++){
+            for (j = 1; j < IdxMax; j++){
+                this.Mat[i][j] = 0;
+            }
+        }
+
+        Brs = 0;
+        Kol = 0;
+    }
+    
 
     
 }
