8bc1e3a6ac72f96ce47fdb3175d9717268d79e7c
leomatt547 <leo.matt.547@gmail.com>
2020-09-22 10:32:05 +0700

    Added fungsi Pokok Leonard

--- src/Matriks.java
+++ src/Matriks.java
@@ -15,7 +15,8 @@ public class matriks {
     int Kol;
     // Deklarasi Matriks
     float[][] Mat = new float[IdxMax][IdxMax];
-    float[][] Tampung = new float[IdxMax][IdxMax];
+    float[][] Temp = new float[IdxMax][IdxMax];
+    float[][] MatSPL = new float [IdxMax][IdxMin];
 
     //Scanner
     Scanner input = new Scanner(System.in);
@@ -35,7 +36,79 @@ public class matriks {
         Brs = 0;
         Kol = 0;
     }
-    
 
+    void bacaMatriksBalikanSPL(){
+        int i,j;
+
+        void bacaUkuranMatriks();
+        for (i = 1; i <= Brs; i++){
+            for (j = 1; j <= Kol; j++){
+                this.Mat[i][j] = input.nextFloat();
+            }
+        }
+    }
+
+    void bacaMatriksBalikan(){
+        int i,j;
+
+        void bacaUkuranMatriks();
+        for (i = 1; i <= Brs; i++){
+            for (j = 1; j <= Kol; j++){
+                this.Mat[i][j] = input.nextFloat();
+            }
+        }
+    }
+
+    void bacaMatriksRegresi(){
+        int i,j;
+
+        void bacaUkuranMatriks();
+        for (i = 1; i <= Brs; i++){
+            for (j = 1; j <= Kol; j++){
+                this.Mat[i][j] = input.nextFloat();
+            }
+        }
+    }
+
+    float pangkat(float x,int i)
+    {
+        int j;
+        float temp;
+
+        xtemp = x;
+        for (j=1 ; j <= i-1; j++)
+        {
+            x = x * temp;
+        }
+        return x;
+    }
+
+    float kaliDiagonal(){
+        float hasil;
+        for (i = 1; i <= Kol; i++){
+            hasil = hasil * this.Mat[i][i];
+        }
+        return hasil;
+    }
+
+    void tulisMatriks(){
+        int i,j;
+
+        for (i = 1; i <= Brs; i++){
+            for (j = 1; j < Kol; j++){
+                System.out.print(this.Mat[i][j] + " ");
+            }
+            System.out.println(this.Mat[i][Kol]);
+        }
+    }
     
-}
+    void TukarBaris(int a, int b){
+        int c;
+        float temp;
+
+        for (c = 1;c <= Kol;c++){
+            temp = this.Mat[b][c];
+            this.Mat[b][c] = this.Mat[a][c];
+            this.Mat[a][c] = temp;
+        }
+    }
