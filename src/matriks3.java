667d361fa53a6f92abcb0f40429285058948493f
Christian Gunawan <57822261+ChristianGunawan@users.noreply.github.com>
2020-09-24 21:21:23 +0700

    By Christian
    masih kurang :
    void bacaFileExtDeterminan()
    void bacaFileExtBalikan()

---
+++ src/matriks3.java
@@ -0,0 +1,280 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class matriks3 {
+    int IdxMax = 15;
+    int Brs;
+    int Kol;
+    float[][] Mat = new float[IdxMax][IdxMax];
+    float[][] Tampung = new float[IdxMax][IdxMax];
+
+    void bacaUkuranMatriks(){
+        //Menerima input banyaknya baris dan banyaknya kolom dari suatu matriks
+            System.out.print("Masukan banyaknya baris : ");
+            Brs = keyboard.nextInt();
+            System.out.print("Masukan banyaknya kolom : ");
+            Kol = keyboard.nextInt();
+        }
+
+    void bacaFileExtSPL(){
+		BufferedReader br = null;
+		FileReader fr = null;
+		int x,dgt,i,d,j;
+		float temp,dtemp;
+		boolean dec,min;
+
+		try {
+
+			//br = new BufferedReader(new FileReader(FILENAME));
+			fr = new FileReader("matriksSPL.txt");
+			br = new BufferedReader(fr);
+
+			String sCurrentLine;
+
+			sCurrentLine = br.readLine();
+
+			if ((sCurrentLine) == null){
+				System.out.println("File Kosong");
+			}
+			else{
+				i=0;
+				j=0;
+				Kol = 0;
+				min=false;
+
+				while ((sCurrentLine) != null){						//asumsikan antar elemen matriks pada file eksternal hanya dipisahkan satu spasi
+					if (min){
+						this.Mat [i][j]=(this.Mat [i][j])*(-1);
+					}
+					dec = false;
+					min = false;
+					j = 1;
+					temp = 0;
+					dgt = 1;
+					i++;
+					for (x=0;x<=(sCurrentLine.length())-1;x++){
+						if ((sCurrentLine.charAt(x))!= ' '){
+							if ((sCurrentLine.charAt(x))== '.'){
+								dec=true;
+								dgt=1;
+							}
+							else if ((sCurrentLine.charAt(x))== '-'){
+								min=true;
+							}
+							else
+							{
+								dtemp=(sCurrentLine.charAt(x))-'0';
+								if (dec){
+									for (d=1;d<=dgt;d++){
+										dtemp=dtemp/10;
+									}
+									dgt++;
+									temp=temp+dtemp;
+									this.Mat[i][j] = temp;
+								}
+								else{
+									temp=(temp*10)+dtemp;
+									this.Mat[i][j] = temp;
+								}
+							}
+						}
+						else{
+							dec = false;
+							temp = 0;
+							if (min){
+								this.Mat [i][j]=(this.Mat [i][j])*-1;
+							}
+							j++;
+							min=false;
+						}
+					}
+					sCurrentLine = br.readLine();
+					if (j>Kol){
+						Kol = j;
+					}
+				}
+				if (min){
+					this.Mat [i][j]=(this.Mat [i][j])*-1;
+				}
+				Brs=i;
+			}
+
+		}
+
+		catch (IOException e) {
+
+			e.printStackTrace();
+			System.out.println("Ada kesalahan pada file eksternal.");
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+	}
+    void bacaFileExtInterpolasi(){
+		BufferedReader br = null;
+		FileReader fr = null;
+		int x,dgt,i,d,j;
+		float temp,dtemp;
+		boolean dec,min;
+
+		try {
+
+			//br = new BufferedReader(new FileReader(FILENAME));
+			fr = new FileReader("matriksInterpolasi.txt");
+			br = new BufferedReader(fr);
+
+			String sCurrentLine;
+
+			sCurrentLine = br.readLine();
+
+			if ((sCurrentLine) == null){
+				System.out.println("File Kosong");
+			}
+			else{
+				i=0;
+				j=0;
+				Kol = 0;
+				min=false;
+
+				while ((sCurrentLine) != null){						//asumsikan antar elemen matriks pada file eksternal hanya dipisahkan satu spasi
+					if (min){
+						this.Mat [i][j]=(this.Mat [i][j])*(-1);
+					}
+					dec = false;
+					min = false;
+					j = 1;
+					temp = 0;
+					dgt = 1;
+					i++;
+					for (x=0;x<=(sCurrentLine.length())-1;x++){
+						if ((sCurrentLine.charAt(x))!= ' '){
+							if ((sCurrentLine.charAt(x))== '.'){
+								dec=true;
+								dgt=1;
+							}
+							else if ((sCurrentLine.charAt(x))== '-'){
+								min=true;
+							}
+							else
+							{
+								dtemp=(sCurrentLine.charAt(x))-'0';
+								if (dec){
+									for (d=1;d<=dgt;d++){
+										dtemp=dtemp/10;
+									}
+									dgt++;
+									temp=temp+dtemp;
+									this.Mat[i][j] = temp;
+								}
+								else{
+									temp=(temp*10)+dtemp;
+									this.Mat[i][j] = temp;
+								}
+							}
+						}
+						else{
+							dec = false;
+							temp = 0;
+							if (min){
+								this.Mat [i][j]=(this.Mat [i][j])*-1;
+							}
+							j++;
+							min=false;
+						}
+					}
+					sCurrentLine = br.readLine();
+				}
+				if (min){
+					this.Mat [i][j]=(this.Mat [i][j])*-1;
+				}
+				Kol=2;
+				Brs=i;
+			}
+
+		}
+
+		catch (IOException e) {
+
+			e.printStackTrace();
+			System.out.println("Ada kesalahan pada file eksternal.");
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+    }
+    void buatKolomNolBawah(int j, int i){
+        //Pivot di M[i][j]
+        //Membuat kolom j nol dimulai dari baris ke i + 1
+            int k = i+1;
+            int l;
+            float factor;
+    
+            while (k <= Brs){
+                factor = this.Mat[k][j];
+                for (l = 1;l <= Kol;l++){
+                    this.Mat[k][l] = this.Mat[k][l] - factor*this.Mat[i][l];
+                }
+                k += 1;
+            }
+        }
+    void buatKolomNolAtas(int i, int j){
+        // Membuat kolom j berisi nol semua diatas indeks i
+            int k = i - 1;
+            int l;
+            float factor;
+    
+            while (k >= 1){
+                factor = this.Mat[k][j];
+                for (l = 1;l <= Kol;l++){
+                    this.Mat[k][l] = this.Mat[k][l] - factor*this.Mat[i][l];
+                }
+                k -= 1;
+                }
+            }
+
+    int indeksPivot(int i){
+    //Mengembalikan indeks pivot point pada baris i
+    //Dengan asumsi bukan baris yang berisi 0 semua (isBarisNol = false)
+        int k = 1;
+        boolean cek = true;
+
+        while (cek & k < Kol){
+            if (this.Mat[i][k] == 0){
+                k += 1;
+            } else {
+                cek = false;
+            }
+        } //
+        return k;
+    }
+}
+
