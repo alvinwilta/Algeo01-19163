6d4f6ea13a15d0b9822ae129ad181e133105009f
Christian Gunawan <57822261+ChristianGunawan@users.noreply.github.com>
2020-10-02 16:26:28 +0700

    VOID CRAMER RULE, HASIL SPL, PRIVATE hasilSPL

--- src/matriks3.java
+++ src/matriks3.java
@@ -570,5 +570,66 @@ public class matriks3 {
 			return this.Kol;
 		}
 	}
+ public void solveCramerRule() {
+        if (this.Brs != this.Kol - 1) {
+            System.out.println("Tidak bisa diselesaikan dengan kaidah Cramer!");
+            this.solution = false;
+            return;
+        }
 
+        // Memisahkan konstanta (b) dan koefisien variabel (A) persamaan
+        double[] konstantaPersamaan = new double[this.Kol + 1];
+        Matriks koefisienVariabel = new Matriks(this.Brs, this.Kol - 1);
+
+        for (int i = 1; i <= this.Brs; i++) {
+            for (int j = 1; j <= this.Kol; j++) {
+                if (j == this.Kol) {
+                    konstantaPersamaan[i] = this.Mat[i][j];
+                } else {
+                    koefisienVariabel.data[i][j] = this.Mat[i][j];
+                }
+            }
+        }
+
+        // Mencari determinan matriks koefisien variabel
+        double determinanKoefisienVariabel = koefisienVariabel.determinantLaplaceExpansion();
+
+        if (determinanKoefisienVariabel == 0) {
+            System.out.println("Tidak bisa diselesaikan dengan kaidah Cramer! (Tidak memiliki solusi unik)");
+            this.solution = false;
+            return;
+        }
+
+        // iterasi tiap Ai pada kaidah Cramer
+        for (int kolomTukar = 1; kolomTukar <= this.Kol; kolomTukar++) {
+            Matriks tempMatrix = new Matriks(koefisienVariabel.Brs, koefisienVariabel.Kol;
+
+            for (int i = 1; i <= koefisienVariabel.getBrs(); i++) {
+                for (int j = 1; j <= koefisienVariabel.getKol(); j++) {
+                    if (j == kolomTukar) {
+                        // Tukar kolom dengan konstanta persamaan (b)
+                        tempMatrix.data[i][j] = konstantaPersamaan[i];
+                    } else {
+                        tempMatrix.data[i][j] = koefisienVariabel.data[i][j];
+                    }
+                }
+            }
+
+            // det(Ai)
+            double tempDeterminant = tempMatrix.determinantLaplaceExpansion();
+
+            this.hasilSPL[kolomTukar] = tempDeterminant / determinanKoefisienVariabel;
+        }
+
+        this.solution = true;
+        System.out.println(this.hasilSPL());
+    }
+    public String hasilSPL() {
+        String hasil = "";
+        for (int i = 1; i <= this.Kol-1; i++) {
+            hasil += String.format("x%d = %f; ", i, this.hasilSPL[i]);
+        }
+        return hasil;
+    }
+    private double[] hasilSPL;
 }
